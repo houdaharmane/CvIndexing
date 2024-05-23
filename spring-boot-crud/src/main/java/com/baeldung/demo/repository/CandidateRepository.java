783fbdce41210828d1a659509fd410cf0f274d06
Houda Harmane <houdaharmane2004@gmail.com>
2024-05-23 15:08:05 +0200

    change project structure

--- spring-boot-crud/src/main/java/com/baeldung/demo/repository/CandidateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.demo.repository;
-
-import com.baeldung.demo.model.Candidate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
-
-}
