312a01bccbc8a7c763b8a79a2bb4f7d8bb4b732a
H.Hr <132271300+houdaharmane@users.noreply.github.com>
2024-06-20 23:03:17 +0200

    Update CandidateRepository.java

--- spring-boot-crud/src/main/java/com/baeldung/demo/repository/CandidateRepository.java
+++ spring-boot-crud/src/main/java/com/baeldung/demo/repository/CandidateRepository.java
@@ -2,7 +2,15 @@ package com.baeldung.demo.repository;
 
 import com.baeldung.demo.model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
+
+    Candidate findByEmail(String email);
+
+    @Query("from Candidate where fileNames in (:filenames)")
+    List<Candidate> findCandidateByFileNameList(List<String> filenames);
 
 }
