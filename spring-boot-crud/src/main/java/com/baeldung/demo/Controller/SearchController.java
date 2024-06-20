6afc56ac3beb3a30ea8fcc370217a55d7cd24f82
H.Hr <132271300+houdaharmane@users.noreply.github.com>
2024-06-20 22:42:36 +0200

    Update SearchController.java

--- spring-boot-crud/src/main/java/com/baeldung/demo/Controller/SearchController.java
+++ spring-boot-crud/src/main/java/com/baeldung/demo/Controller/SearchController.java
@@ -1,11 +1,16 @@
 package com.baeldung.demo.Controller;
 
+import com.baeldung.demo.components.CandidateServiceImpl;
 import com.baeldung.demo.model.Candidate;
 import com.baeldung.demo.service.CandidateService;
+import com.baeldung.demo.service.KeywordSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -13,7 +18,13 @@ import java.util.List;
 public class SearchController {
 
     @Autowired
-    private CandidateService candidateService;
+    private CandidateServiceImpl candidateService;
+
+    @Autowired
+    private KeywordSearchService keywordSearchService;
+
+
+
 
     @GetMapping("/chercher")
     public String showSearchPage(Model model) {
@@ -21,4 +32,19 @@ public class SearchController {
         model.addAttribute("candidates", candidates);
         return "search";
     }
+
+    @PostMapping("/chercher")
+    public String search(@RequestParam("keyword") String keyword, Model model) {
+        List<Candidate> candidates = keywordSearchService.findByKeyword(keyword);
+        model.addAttribute("candidates", candidates);
+        return "search";
+    }
+    @PostMapping("/createCandidate")
+    public String createCandidate(@RequestParam("firstName") String firstName,
+                                  @RequestParam("lastName") String lastName,
+                                  @RequestParam("files") MultipartFile[] files) {
+        candidateService.save(firstName, lastName, files);
+        return "redirect:/chercher";
+    }
+
 }
