b7b9e3cddad3e9e9fee04373139b84e20c9c84e4
H.Hr <132271300+houdaharmane@users.noreply.github.com>
2024-06-20 22:45:47 +0200

    Update Resumes.java

--- spring-boot-crud/src/main/java/com/baeldung/demo/model/Resumes.java
+++ spring-boot-crud/src/main/java/com/baeldung/demo/model/Resumes.java
@@ -1,38 +1,52 @@
 package com.baeldung.demo.model;
 
-public class Resumes {
-    private String content;
-    private ResumeDetails resumeDetails;
-    private FileDetails fileDetails;
+import org.springframework.data.elasticsearch.annotations.Document;
 
+import java.util.Map;
+
+@Document(indexName = "resumes")
+public class Resumes {
+
+
+    private  String id;
+    private String content;
+    private Map<String, Object> meta;
+    private Map<String, Object> file;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
     public String getContent() {
         return content;
     }
 
     public void setContent(String content) {
-        this.content=content;
+        this.content = content;
     }
 
-    public ResumeDetails getResumeDetails() {
-        return resumeDetails;
+    public Map<String, Object> getMeta() {
+        return meta;
     }
 
-    public void setResumeDetails(ResumeDetails resumeDetails) {
-        this.resumeDetails = resumeDetails;
+    public void setMeta(Map<String, Object> meta) {
+        this.meta = meta;
     }
 
-    public FileDetails getFileDetails() {
-        return fileDetails;
+    public Map<String, Object> getFile() {
+        return file;
     }
 
-    public void setFileDetails(FileDetails fileDetails) {
-        this.fileDetails = fileDetails;
+    public void setFile(Map<String, Object> file) {
+        this.file = file;
     }
 
     public static class ResumeDetails {
         private String fileTitle;
         private String nameCandidate;
-
 
         public String getFileTitle() {
             return fileTitle;
@@ -52,16 +66,14 @@ public class Resumes {
     }
 
     public static class FileDetails {
-        private String ExtensionFile;
+        private String extension;
 
-
-        public String getExtensionFile() {
-            return ExtensionFile;
+        public String getExtension() {
+            return extension;
         }
 
-        public void setExtensionFile(String extensionFile) {
-            this.ExtensionFile = extensionFile;
+        public void setExtension(String extension) {
+            this.extension = extension;
         }
     }
 }
-
